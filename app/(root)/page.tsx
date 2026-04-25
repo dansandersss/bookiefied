60689592a055fd070f1fe6cea51a747b4dd20b55
Daniel Sandrs <danielsandrs@MacBook-Pro-Daniel.local>
2026-04-25 17:29:08 +0300

    added Hero Section and Search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-// import HeroSection from "@/components/HeroSection";
+import HeroSection from "@/components/HeroSection";
 import BookCard from "@/components/BookCard";
 import {getAllBooks} from "@/lib/actions/book.actions";
-// import Search from "@/components/Search";
+import Search from "@/components/Search";
 
 const Page = async ({ searchParams }: { searchParams: Promise<{ query?: string }> }) => {
     const { query } = await searchParams;
@@ -12,12 +12,12 @@ const Page = async ({ searchParams }: { searchParams: Promise<{ query?: string }
 
     return (
         <main className="wrapper container">
-            {/*<HeroSection />*/}
+            <HeroSection />
 
-            {/*<div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">*/}
-            {/*    <h2 className="text-3xl font-serif font-bold text-[#212a3b]">Recent Books</h2>*/}
-            {/*    <Search />*/}
-            {/*</div>*/}
+            <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-5 mb-10">
+                <h2 className="text-3xl font-serif font-bold text-[#212a3b]">Recent Books</h2>
+                <Search />
+            </div>
 
             <div className="library-books-grid">
                 {books.map((book) => (
